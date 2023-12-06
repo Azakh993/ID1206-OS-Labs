aace7fb7fd44213921b06da70c75a85e21ee4000
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-07 00:31:24 +0100

    Implemented algorithm. Preliminary tests indicate correct functionality.

---
+++ Lab_4/SCAN.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def find_closest_request(head, requests, direction):
+    closest_request = None
+    shortest_distance = sys.maxsize
+
+    for request in requests:
+        distance = abs(head - request)
+
+        if (direction == 'L' and head >= request) or (direction == 'R' and head <= request):
+            if distance < shortest_distance:
+                closest_request = request
+                shortest_distance = distance
+
+    return shortest_distance, closest_request
+
+
+def switch_direction(direction):
+    return 'R' if direction == 'L' else 'L'
+
+
+def scan(head, requests):
+    requests_copy = requests[:]
+
+    total_head_movement = 0
+    direction = 'L'
+
+    while requests_copy:
+        distance, request = find_closest_request(head, requests_copy, direction)
+
+        if request is None:
+            direction = switch_direction(direction)
+            total_head_movement += abs(head)
+            head = 0
+        else:
+            total_head_movement += distance
+            head = request
+            requests_copy.remove(request)
+
+    print("SCAN: ", total_head_movement)
