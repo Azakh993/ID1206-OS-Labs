38c26f2426b01f4c5b17fd1e88ce7affabcae99a
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-07 16:25:50 +0100

    Added code for generating random number and receiving head via argument.

--- Lab_4/main.py
+++ Lab_4/main.py
@@ -1,17 +1,48 @@
+import random
+import sys
+from CLOOK import clook
 from CSCAN import cscan
 from FCFS import fcfs
+from LOOK import look
 from SCAN import scan
 from SSTF import sstf
 
 
+def get_script_argument():
+    if len(sys.argv) != 2:
+        print("Usage: python your_script.py <head_value>")
+        return
+
+    try:
+        head = int(sys.argv[1])
+    except ValueError:
+        print("Invalid head value. Please enter a valid integer.")
+        return
+
+    return head
+
+
+def generate_random_numbers(number_of_random_numbers, upper_bound):
+    random_numbers = []
+    for i in range(number_of_random_numbers):
+        random_number = random.randint(0, upper_bound)
+        random_numbers.append(random_number)
+    return random_numbers
+
+
 def main():
-    requests = [176, 79, 34, 60, 92, 11, 41, 114]
-    head = 50
+    head = get_script_argument()
+
+    number_of_random_numbers = 1000
+    upper_bound = 4999
+    requests = generate_random_numbers(number_of_random_numbers, upper_bound)
 
     fcfs(head, requests)
     sstf(head, requests)
-    scan(head, requests)
-    cscan(head, requests)
+    scan(head, requests, upper_bound)
+    cscan(head, requests, upper_bound)
+    look(head, requests)
+    clook(head, requests)
 
 
 if __name__ == "__main__":
