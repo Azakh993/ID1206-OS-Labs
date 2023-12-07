f8a07173e52b34e7a5e63584425970981726c45d
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-07 15:36:28 +0100

    Modified to move to end of disk.

--- Lab_4/CSCAN.py
+++ Lab_4/CSCAN.py
@@ -16,20 +16,20 @@ def find_closest_request(head, requests):
     return shortest_distance, closest_request
 
 
-def cscan(head, requests):
-    requests_copy = requests[:]
+def cscan(head, requests, upper_bound):
+    requests_2 = requests[:]
 
     total_head_movement = 0
 
-    while requests_copy:
-        distance, request = find_closest_request(head, requests_copy)
+    while requests_2:
+        distance, request = find_closest_request(head, requests_2)
 
         if request is None:
-            total_head_movement += abs(head)
+            total_head_movement += abs(head - upper_bound) + upper_bound
             head = 0
         else:
             total_head_movement += distance
             head = request
-            requests_copy.remove(request)
+            requests_2.remove(request)
 
     print("C-SCAN: ", total_head_movement)
